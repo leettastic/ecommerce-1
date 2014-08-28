bb1280ede2e1570a0390dcfb986ec94d67357bc3
Adrian Emil Cendrowski <leettastic@gmail.com>
2014-08-28 10:54:26 +0100

    Behat init

--- src/Sellsei/EcommerceCoreBundle/Resources/public/js/app.js
+++ src/Sellsei/EcommerceCoreBundle/Resources/public/js/app.js
@@ -1,6 +1,7 @@
 "use strict";
 angular.module("sellseiEcommerceApp", ["ngRoute"])
 .config(['$routeProvider', '$interpolateProvider', function ($routeProvider, $interpolateProvider) {
+    $routeProvider.when("/", { templateUrl: Routing.generate('sellsei_ecommerce_core_homepage', {}, true) });
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
 }])
